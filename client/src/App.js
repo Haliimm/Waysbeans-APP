639c69ee97e1220e042b418593f0c6b296527151
Haliimm <halimawaludienkhafifie@gmail.com>
2023-03-20 09:14:00 +0700

    Add Routing

--- client/src/App.js
+++ client/src/App.js
@@ -10,22 +10,33 @@ import AdminDashboard from "./pages/AdminDashboard";
 import ListProductAdmin from "./pages/ListProductAdmin";
 import UpdateProfilePage from "./pages/UpdateProfilePage";
 import Header from "./component/Header";
+import { UserRoute, AdminRoute } from "./component/PrivateRoutes";
+import { UserContext } from "./context/userContext";
+import React, { useContext } from "react";
 
 function App() {
+  const [state] = useContext(UserContext);
 
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={state.user.is_admin ? <AdminDashboard /> : <Home />} />
         <Route path="/detail-product/:id" element={<DetailProduct />} />
-        <Route path="/my-cart" element={<MyCart />} />
-        <Route path="/my-transaction" element={<MyTransaction />} />
-        <Route path="/add-product" element={<AddCard />} />
-        <Route path="/update-product/:id" element={<UpdateCard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/list-product" element={<ListProductAdmin />} />
-        <Route path="/update-profile" element={<UpdateProfilePage />} />
+
+        {/* Route User */}
+        <Route path="/" element={<UserRoute />}>
+          <Route path="/my-cart" element={<MyCart />} />
+          <Route path="/my-transaction" element={<MyTransaction />} />
+          <Route path="/update-profile" element={<UpdateProfilePage />} />
+        </Route>
+
+        {/* Route Admin */}
+        <Route path="/" element={<AdminRoute />}>
+          <Route path="/add-product" element={<AddCard />} />
+          <Route path="/update-product/:id" element={<UpdateCard />} />
+          <Route path="/list-product" element={<ListProductAdmin />} />
+        </Route>
       </Routes>
     </div>
   );
